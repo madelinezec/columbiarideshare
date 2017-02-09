50466eb9cc9d1a59c115da9c0b2da52f723add77
madelinezec <mez2113@columbia.edu>
2017-02-08 23:07:25 -0500

    god damn it

---
+++ app.js
@@ -0,0 +1,16 @@
+    function.googleSignin(){
+       var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+    }
+
+    firebase.auth().getRedirectResult().then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            var user = result.user;
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+    });
+
