839ed47843bf0891b8cbe62cea5c3afcc303b08a
madelinezec <mez2113@columbia.edu>
2017-02-25 23:35:00 -0500

    can write to databse correctly

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-    function initDatabase(){
-        var database = firebase.database();
-	//var ref = firebase.database.ref();
-
-    }
-    
     function googleSignin(){
         event.preventDefault();
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithRedirect(provider);
-        console.log('bitch ok');
     }
 
     firebase.auth().getRedirectResult().then(function(result) {
-        console.log('bitch okkkrr');
         if (result.credential) {
     // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
@@ -30,31 +22,31 @@
            var credential = error.credential;
           // ...
        });
-
-
+ 
        function submitRidePressed(){
         event.preventDefault();
-        var provider = new firebase.auth.GoogleAuthProvider();
            var e = document.getElementById("airport");
            var airportSelect = e.options[e.selectedIndex].text;
            
            var date = $("#datepicker").datepicker('getDate');
-           
+           var res = String(date);
+           console.log(res);
+           res = res.substr(0,15);
+           console.log('new res' + res);
 	   var fromTime = $("#startTime").val();
 	   var toTime = $("#endTime").val();
 	   
 	   console.log(airportSelect);
-           console.log(date);
 	   console.log(fromTime + ' ' + toTime);
 
-	   writeRide(airportSelect, date, fromTime, toTime);
+	   writeRide(airportSelect, res, fromTime, toTime);
 
        }
 
        function writeRide(airportSelect, date, startTime, endTime){
-           firebase.database().ref().set({
-               aiport: airportSelect,
-	       day: date, 
+           firebase.database().ref('airport/' + airportSelect).set({
+               //aiport: airportSelect,
+	       when: date, 
 	       from: startTime, 
 	       to: endTime
 	   });
