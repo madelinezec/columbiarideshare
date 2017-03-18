fa455f50713c4cb09452f7f0f7bbdbde122fd792
madelinezec <mez2113@columbia.edu>
2017-03-18 14:51:28 -0400

    create initApp function to get current user

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-    function googleSignin(){
+ function initApp() {
+     this.auth = firebase.auth();
+       this.database = firebase.database();
+	 console.log('initApp called');
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            currentUser = user;
+	    console.log(user);
+	    console.log('success');
+        //
+        }else{
+         //  
+	}
+    });
+   }
+
+
+ function googleSignin(){
         event.preventDefault();
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithRedirect(provider);
     }
 
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-    // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-        }).catch(function(error) {
-       // Handle Errors here.
-           var errorCode = error.code;
-           var errorMessage = error.message;
-          // The email of the user's account used.
-           var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-           var credential = error.credential;
-          // ...
-       });
- 
        function rideToPressed(){
         event.preventDefault();
 	console.log("correct you got here");
@@ -41,16 +41,20 @@
 	   console.log(fromTime + ' ' + toTime);
 	   var text = document.getElementById('comment').value;
 	   console.log(text);
-	   writeRideTo(airportSelect, res, fromTime, toTime, text);
+	   
+	   writeRideTo(airportSelect, currentUser.displayName, currentUser.email, currentUser.photoURL, res, fromTime, toTime, text);
 
        }
 
-       function writeRideTo(airportSelect, date, startTime, endTime, text){
+       function writeRideTo(airportSelect, userName, userEmail, photoID, date, startTime, endTime, text){
           var ref = firebase.database().ref();
           var postsRef = ref.child('TOairport/' + airportSelect); 
           var newPostRef = postsRef.push(); 
           newPostRef.set({
                //aiport: airportSelect,
+	       user: userName,
+	       email: userEmail,
+	       id: photoID,
 	       when: date, 
 	       from: startTime, 
 	       to: endTime,
