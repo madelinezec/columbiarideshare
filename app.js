fee3b86a533480806ca63c99341c098847f5a595
madelinezec <mez2113@columbia.edu>
2017-03-29 17:38:24 -0400

    cleaned up indentation

--- app.js
+++ app.js
@@ -1,213 +1,273 @@
  function initApp() {
      this.auth = firebase.auth();
-       this.database = firebase.database();
-	 console.log('initApp called');
-      firebase.auth().getRedirectResult().then(function(result) {
-        //    document.getElementById('quickstart-sign-in').textContent = 'LOG OUT';
-        //    console.log('changing log out value');
+     this.database = firebase.database();
+     console.log('initApp called');
+     firebase.auth().getRedirectResult().then(function(result) {
+         //    document.getElementById('quickstart-sign-in').textContent = 'LOG OUT';
+         //    console.log('changing log out value');
          //   document.getElementById('quickstart-sign-in').addEventListener('click', googleSignOut);
-        if (result.credential) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // [START_EXCLUDE]
-         console.log('you got access token');
-         
-        } else {
-          console.log('you dont have access token');
-          // [END_EXCLUDE]
-        }
-        // The signed-in user info.
-        var user = result.user;
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // [START_EXCLUDE]
-        if (errorCode === 'auth/account-exists-with-different-credential') {
-          alert('You have already signed up with a different auth provider for that email.');
-          // If you are using multiple auth providers on your app you should handle linking
-          // the user's accounts here.
-        } else {
-          console.error(error);
-        }
-        // [END_EXCLUDE]
-      });
-      // Listening for auth state changes.
-      // [START authstatelistener]
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-           
-            window.currentUser = user;
-	    console.log(user);
-	    console.log('success');
-	    document.getElementById('quickstart-sign-in').textContent = 'LOG OUT';
-	    document.getElementById('quickstart-sign-in').addEventListener('click', googleSignOut);
-          return true;
-        }else{
-	document.getElementById('quickstart-sign-in').textContent = 'LOGIN';
-	document.getElementById('quickstart-sign-in').addEventListener('click', 
-	function(){
-	    document.getElementById('id01').style.display='block';
-	    });
-	
-         // 
-         return false; 
-	}
-    });
+         if (result.credential) {
+             // This gives you a Google Access Token. You can use it to access the Google API.
+             var token = result.credential.accessToken;
+             // [START_EXCLUDE]
+             console.log('you got access token');
 
-         //delete old posts
-       var ref = new firebase.database().ref('TOairport/JFK');
-       var now = Date.now();
-       var cutoff = now;
-       var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
-       var listener = old.on('child_added', function(snapshot){
-           console.log('DELETING');
-	   var string = snapshot.val().reference;
-	   console.log(string);
-	   deleteMessage(string);
-	   snapshot.ref.remove();
-       });
- 
-       console.log('done done done');
+         } else {
+             console.log('you dont have access token');
+             // [END_EXCLUDE]
+         }
+         // The signed-in user info.
+         var user = result.user;
+     }).catch(function(error) {
+         // Handle Errors here.
+         var errorCode = error.code;
+         var errorMessage = error.message;
+         // The email of the user's account used.
+         var email = error.email;
+         // The firebase.auth.AuthCredential type that was used.
+         var credential = error.credential;
+         // [START_EXCLUDE]
+         if (errorCode === 'auth/account-exists-with-different-credential') {
+             alert('You have already signed up with a different auth provider for that email.');
+             // If you are using multiple auth providers on your app you should handle linking
+             // the user's accounts here.
+         } else {
+             console.error(error);
+         }
+         // [END_EXCLUDE]
+     });
+     // Listening for auth state changes.
+     // [START authstatelistener]
+     firebase.auth().onAuthStateChanged(function(user) {
+         if (user) {
 
-   }
- 
- function googleSignOut(){
-    console.log('sign out called');
-    firebase.auth().signOut();
-  window.location = "index.html";
+             window.currentUser = user;
+             console.log(user);
+             console.log('success');
+             document.getElementById('quickstart-sign-in').textContent = 'LOG OUT';
+             document.getElementById('quickstart-sign-in').addEventListener('click', googleSignOut);
+             return true;
+         } else {
+             document.getElementById('quickstart-sign-in').textContent = 'LOGIN';
+             document.getElementById('quickstart-sign-in').addEventListener('click',
+                 function() {
+                     document.getElementById('id01').style.display = 'block';
+                 });
+
+             // 
+             return false;
+         }
+     });
+
+     //delete old posts
+     var ref = new firebase.database().ref('TOairport/JFK');
+     var now = Date.now();
+     var cutoff = now;
+     var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
+     var listener = old.on('child_added', function(snapshot) {
+         console.log('DELETING');
+         var string = snapshot.val().reference;
+         console.log(string);
+         deleteMessage(string);
+         snapshot.ref.remove();
+     });
+
+     var ref = new firebase.database().ref('TOairport/Newark');
+     var now = Date.now();
+     var cutoff = now;
+     var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
+     var listener = old.on('child_added', function(snapshot) {
+         console.log('DELETING');
+         var string = snapshot.val().reference;
+         console.log(string);
+         deleteMessage(string);
+         snapshot.ref.remove();
+     });
+
+     var ref = new firebase.database().ref('TOairport/Laguardia');
+     var now = Date.now();
+     var cutoff = now;
+     var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
+     var listener = old.on('child_added', function(snapshot) {
+         console.log('DELETING');
+         var string = snapshot.val().reference;
+         console.log(string);
+         deleteMessage(string);
+         snapshot.ref.remove();
+     });
+     var ref = new firebase.database().ref('FROMairport/JFK');
+     var now = Date.now();
+     var cutoff = now;
+     var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
+     var listener = old.on('child_added', function(snapshot) {
+         console.log('DELETING');
+         var string = snapshot.val().reference;
+         console.log(string);
+         deleteMessage(string);
+         snapshot.ref.remove();
+     });
+     var ref = new firebase.database().ref('FROMairport/Newark');
+     var now = Date.now();
+     var cutoff = now;
+     var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
+     var listener = old.on('child_added', function(snapshot) {
+         console.log('DELETING');
+         var string = snapshot.val().reference;
+         console.log(string);
+         deleteMessage(string);
+         snapshot.ref.remove();
+     });
+     var ref = new firebase.database().ref('FROMairport/Laguardia');
+     var now = Date.now();
+     var cutoff = now;
+     var old = ref.orderByChild('rideDate').endAt(now).limitToLast(1);
+     var listener = old.on('child_added', function(snapshot) {
+         console.log('DELETING');
+         var string = snapshot.val().reference;
+         console.log(string);
+         deleteMessage(string);
+         snapshot.ref.remove();
+     });
+
+     console.log('done done done');
+
  }
 
- function googleSignin(){
-    console.log('you get called');
-	    event.preventDefault();
-	    var provider = new firebase.auth.GoogleAuthProvider();
+ function googleSignOut() {
+     console.log('sign out called');
+     firebase.auth().signOut();
+     window.location = "index.html";
+ }
 
-            firebase.auth().signInWithRedirect(provider);
-    }
+ function googleSignin() {
+     console.log('you get called');
+     event.preventDefault();
+     var provider = new firebase.auth.GoogleAuthProvider();
 
-function facebookSignIn(){
-    event.preventDefault();
-    var provider = new firebase.auth.FacebookAuthProvider();
-    firebase.auth().signInWithRedirect(provider);
+     firebase.auth().signInWithRedirect(provider);
+ }
 
-}
+ function facebookSignIn() {
+     event.preventDefault();
+     var provider = new firebase.auth.FacebookAuthProvider();
+     firebase.auth().signInWithRedirect(provider);
 
-   function deleteMessage(msgToken){
-       var ref = new firebase.database().ref('messages');
-       ref.child(msgToken).remove();
-   }
+ }
 
-   function rideToPressed(){
-       event.preventDefault();
-       if(window.currentUser){    
+ function deleteMessage(msgToken) {
+     var ref = new firebase.database().ref('messages');
+     ref.child(msgToken).remove();
+ }
 
-	console.log("correct you got here");
-           var e = document.getElementById("airport");
-           var airportSelect = e.options[e.selectedIndex].text;
-           if(airportSelect.indexOf(' ') > -1){
-               airportSelect = airportSelect.substring(0, airportSelect.indexOf(' ')); 
-           }
-           var date = $("#datepicker").datepicker('getDate');
-           var res = String(date);
-           console.log(res);
-           res = res.substr(0,15);
-           console.log('new res' + res);
-	   var fromTime = $("#startTime").val();
-	   var toTime = $("#endTime").val();
-	   
-	   console.log(airportSelect);
-	   console.log(fromTime + ' ' + toTime);
-	   var text = document.getElementById('comment').value;
-	   console.log(text);
-	   
-	   writeRideTo(airportSelect, currentUser.displayName, currentUser.email, currentUser.photoURL, res, fromTime, toTime, text);
-          }else{
-              console.log('you are definitely not signed in');
-              document.getElementById('id01').style.display='block';
-              console.log('you are not signed in');
-          }
-          
+ function rideToPressed() {
+     event.preventDefault();
+     if (window.currentUser) {
 
-       }
+         console.log("correct you got here");
+         var e = document.getElementById("airport");
+         var airportSelect = e.options[e.selectedIndex].text;
+         if (airportSelect.indexOf(' ') > -1) {
+             airportSelect = airportSelect.substring(0, airportSelect.indexOf(' '));
+         }
+         var date = $("#datepicker").datepicker('getDate');
+         var res = String(date);
+         console.log(res);
+         res = res.substr(0, 15);
+         console.log('new res' + res);
+         var fromTime = $("#startTime").val();
+         var toTime = $("#endTime").val();
 
-       function writeRideTo(airportSelect, userName, userEmail, photoID, date, startTime, endTime, text){
-          var ref = firebase.database().ref();
-          var postsRef = ref.child('TOairport/' + airportSelect); 
-          var newPostRef = postsRef.push();
-          var postID = newPostRef.key;
-          var dateString = Date.parse(date + ' ' + endTime);
-          console.log(postID); 
-          newPostRef.set({
-               reference: postID,
-	       user: userName,
-	       email: userEmail,
-	       id: photoID,
-	       when: date, 
-	       from: startTime, 
-	       to: endTime,
-	       rideDate: dateString,
-               comments: text
-	   });
-          var file = "to" + airportSelect + ".html";
-          console.log('redirect to here: ' + file);
-          window.location = file; 
-       }
+         console.log(airportSelect);
+         console.log(fromTime + ' ' + toTime);
+         var text = document.getElementById('comment').value;
+         console.log(text);
+
+         writeRideTo(airportSelect, currentUser.displayName, currentUser.email, currentUser.photoURL, res, fromTime, toTime, text);
+     } else {
+         console.log('you are definitely not signed in');
+         document.getElementById('id01').style.display = 'block';
+         console.log('you are not signed in');
+     }
 
 
-function rideFromPressed(){
-           event.preventDefault();
-           if(window.currentUser){
-           console.log("correct you got here");
-           var e = document.getElementById("airport");
-           var airportSelect = e.options[e.selectedIndex].text;
-           
-           if(airportSelect.indexOf(' ') > -1){
-               airportSelect = airportSelect.substring(0, airportSelect.indexOf(' '));
-           }
-           var date = $("#datepicker").datepicker('getDate');
-           var res = String(date);
-           console.log(res);
-           res = res.substr(0,15);
-           console.log('new res' + res);
-           var fromTime = $("#startTime").val();
-           var toTime = $("#endTime").val();
+ }
 
-           console.log(airportSelect);
-           console.log(fromTime + ' ' + toTime);
-           var text = document.getElementById('comment').value;
-           console.log(text);
+ function writeRideTo(airportSelect, userName, userEmail, photoID, date, startTime, endTime, text) {
+     var ref = firebase.database().ref();
+     var postsRef = ref.child('TOairport/' + airportSelect);
+     var newPostRef = postsRef.push();
+     var postID = newPostRef.key;
+     var dateString = Date.parse(date + ' ' + endTime);
+     console.log(postID);
+     newPostRef.set({
+         reference: postID,
+         user: userName,
+         email: userEmail,
+         id: photoID,
+         when: date,
+         from: startTime,
+         to: endTime,
+         rideDate: dateString,
+         comments: text
+     });
+     var file = "to" + airportSelect + ".html";
+     console.log('redirect to here: ' + file);
+     window.location = file;
+ }
 
-           writeRideFrom(airportSelect, currentUser.displayName, currentUser.email, currentUser.photoURL, res, fromTime, toTime, text);
-           }else{
-               document.getElementById('id01').style.display='block';
-               console.log('you are not signed in');
 
-            }
-       }
+ function rideFromPressed() {
+     event.preventDefault();
+     if (window.currentUser) {
+         console.log("correct you got here");
+         var e = document.getElementById("airport");
+         var airportSelect = e.options[e.selectedIndex].text;
 
-       function writeRideFrom(airportSelect, userName, userEmail, photoID, date, startTime, endTime, text){
-          var ref = firebase.database().ref();
-          var postsRef = ref.child('FROMairport/' + airportSelect);
-          var newPostRef = postsRef.push();
-          var postID = newPostRef.key;
-          var dateString = Date.parse(date + ' ' + endTime);
-          newPostRef.set({
-               reference: postID,
-               user: userName,
-               email: userEmail,
-               id: photoID,
-               when: date,
-               from: startTime,
-               to: endTime,
-	       rideDate: dateString,
-               comments: text
-           });
-          var file = "from" + airportSelect + ".html";
-          console.log('redirect to here: ' + file);
-          window.location = file;
-       }
+         if (airportSelect.indexOf(' ') > -1) {
+             airportSelect = airportSelect.substring(0, airportSelect.indexOf(' '));
+         }
+         var date = $("#datepicker").datepicker('getDate');
+         var res = String(date);
+         console.log(res);
+         res = res.substr(0, 15);
+         console.log('new res' + res);
+         var fromTime = $("#startTime").val();
+         var toTime = $("#endTime").val();
+
+         console.log(airportSelect);
+         console.log(fromTime + ' ' + toTime);
+         var text = document.getElementById('comment').value;
+         console.log(text);
+
+         writeRideFrom(airportSelect, currentUser.displayName, currentUser.email, currentUser.photoURL, res, fromTime, toTime, text);
+     } else {
+         document.getElementById('id01').style.display = 'block';
+         console.log('you are not signed in');
+
+     }
+ }
+
+ function writeRideFrom(airportSelect, userName, userEmail, photoID, date, startTime, endTime, text) {
+     var ref = firebase.database().ref();
+     var postsRef = ref.child('FROMairport/' + airportSelect);
+     var newPostRef = postsRef.push();
+     var postID = newPostRef.key;
+     var dateString = Date.parse(date + ' ' + endTime);
+     newPostRef.set({
+         reference: postID,
+         user: userName,
+         email: userEmail,
+         id: photoID,
+         when: date,
+         from: startTime,
+         to: endTime,
+         rideDate: dateString,
+         comments: text
+     });
+     var file = "from" + airportSelect + ".html";
+     console.log('redirect to here: ' + file);
+     window.location = file;
+ }
+
+
+
--- postRides.js
+++ postRides.js
@@ -1,18 +1,18 @@
-    function loadRides(direction, airport){
+    function loadRides(direction, airport) {
         var ref = firebase.database().ref();
-	ref.on("value", function(snapshot) {
-	  console.log(snapshot.val());
-	  }, function (errorObject) {
-	   console.log("The read failed: " + errorObject.code);
-	    });
-          var x = 0;
-	 ref.child(direction + '/' + airport).once("value", function(data) {
-             data.forEach(function(snapshot) {
-             var userName = snapshot.val().user; 
+        ref.on("value", function(snapshot) {
+            console.log(snapshot.val());
+        }, function(errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+        var x = 0;
+        ref.child(direction + '/' + airport).once("value", function(data) {
+            data.forEach(function(snapshot) {
+                var userName = snapshot.val().user;
 
 
 
-$("#ride").append(`
+                $("#ride").append(`
 <div class="style">
     <img src="${snapshot.val().id}" style="width:68px;height:68px;"/>
     <p>${snapshot.val().user}</p>
@@ -22,37 +22,35 @@ $("#ride").append(`
     <button type="button" class="btn btn-outline-info" onclick="initMessenger('${snapshot.val().user}','${snapshot.val().reference}')">join this ride</button>
     </ul>
 </div>`);
-
-	
- console.log(userName);
-       });
-        console.log('ID : ' + $("#ride").html());
-});
-     console.log('ID : ' + $("#ride").html());
-}
+            });
+        });
+    }
 
 
-    function initMessenger(name, reference){
-       console.log('initMessenger called with this reference: ' + reference);
-       if(window.currentUser){
-           console.log('you are signed in');
-           document.getElementById('convoHeader').innerHTML = name;
-           document.getElementById('messages-card').style.display = "";
-	   window.friendlyChat = new FriendlyChat(reference);
-	   
-       }else{
-           document.getElementById('id01').style.display='block';   
-           console.log('you are not signed in');
-       }
+    function initMessenger(name, reference) {
+        console.log('initMessenger called with this reference: ' + reference);
+        if (window.currentUser) {
+            console.log('you are signed in');
+            document.getElementById('convoHeader').innerHTML = name;
+            document.getElementById('messages-card').style.display = "";
+            window.friendlyChat = new FriendlyChat(reference);
 
-    } 
+        } else {
+            document.getElementById('id01').style.display = 'block';
+            console.log('you are not signed in');
+        }
 
-function logConsole(){
- window.onload = function() {
-      
-    console.log('ID : ' + $('.ride').text());
-console.log(inner);
+    }
 
-      console.log('honey im home');
-    };
-}
+    function logConsole() {
+        window.onload = function() {
+
+            console.log('ID : ' + $('.ride').text());
+            console.log(inner);
+
+            console.log('honey im home');
+        };
+    }
+
+
+
